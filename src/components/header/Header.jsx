e596dc91cafb9ca53c977927bc31982d90acd833
thuyy083 <142137343+thuyy083@users.noreply.github.com>
2026-03-13 10:09:55 +0700

    login, logout, profile

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,102 @@
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useState, useRef, useEffect } from "react";
+import { logout } from "../../redux/slices/authSlice";
+import { logoutApi } from "../../services/authService";
+
 import "./Header.scss";
 
 function Header() {
+
+  const user = useSelector((state) => state.auth.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [open, setOpen] = useState(false);
+  const menuRef = useRef();
+
+  const role = user?.roles?.[0]?.name;
+
+  const toggleMenu = () => {
+    setOpen(!open);
+  };
+
+  const handleLogout = async () => {
+    try {
+      await logoutApi();
+    } catch (e) {console.log(e)}
+
+    dispatch(logout());
+    localStorage.removeItem("token");
+
+    navigate("/login");
+  };
+
+  const handleProfile = () => {
+    navigate("/profile");
+  };
+
+  // click ngoài menu sẽ đóng dropdown
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (!menuRef.current?.contains(e.target)) {
+        setOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className="header">
-      <h3>CRM Dashboard</h3>
 
-      <div className="user-info">
-        Admin
+      <h3>HỆ THỐNG QUẢN LÝ TIẾP XÚC DOANH NGHIỆP</h3>
+
+      <div className="user-section" ref={menuRef}>
+
+        <div className="user-info" onClick={toggleMenu}>
+
+          <div className="avatar">
+            {user?.fullName?.charAt(0)}
+          </div>
+
+          <div className="user-text">
+            <span className="name">{user?.fullName}</span>
+            <span className="role">{role}</span>
+          </div>
+
+          <span className={`dropdown-icon ${open ? "rotate" : ""}`}>
+            ▼
+          </span>
+
+        </div>
+
+        {open && (
+          <div className="dropdown-menu">
+
+            <div
+              className="dropdown-item"
+              onClick={handleProfile}
+            >
+              Thông tin cá nhân
+            </div>
+
+            <div
+              className="dropdown-item logout"
+              onClick={handleLogout}
+            >
+              Đăng xuất
+            </div>
+
+          </div>
+        )}
+
       </div>
+
     </div>
   );
 }
