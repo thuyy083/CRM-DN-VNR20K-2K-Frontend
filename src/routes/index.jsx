891bbfdd61c107e8a1528126e152289116b3e46f
thuyy083 <142137343+thuyy083@users.noreply.github.com>
2026-03-13 10:09:55 +0700

    login, logout, profile

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,6 +3,8 @@ import MainLayout from "../layouts/mainLayout/MainLayout";
 
 import Login from "../pages/login/Login";
 import Dashboard from "../pages/dashboard/Dashboard";
+import ProtectedRoute from "./ProtectedRoute";
+import Profile from "../pages/profile/Profile";
 
 function AppRoutes() {
   return (
@@ -12,9 +14,19 @@ function AppRoutes() {
         {/* Public route */}
         <Route path="/login" element={<Login />} />
 
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <MainLayout />
+            </ProtectedRoute>
+          }
+        ></Route>
+
         {/* Protected routes dùng layout */}
         <Route element={<MainLayout />}>
           <Route path="/" element={<Dashboard />} />
+          <Route path="/profile" element={<Profile />} />
         </Route>
 
       </Routes>
