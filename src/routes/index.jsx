7a2c62be19d5c718501ebe55d9baf0cc7355b596
Banh983 <baoanhvn.2003@gmail.com>
2026-03-13 08:35:58 +0700

    init employee interface

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,6 +3,7 @@ import MainLayout from "../layouts/mainLayout/MainLayout";
 
 import Login from "../pages/login/Login";
 import Dashboard from "../pages/dashboard/Dashboard";
+import Employees from "../pages/employees/Employees";
 
 function AppRoutes() {
   return (
@@ -15,6 +16,7 @@ function AppRoutes() {
         {/* Protected routes dùng layout */}
         <Route element={<MainLayout />}>
           <Route path="/" element={<Dashboard />} />
+          <Route path="/employees" element={<Employees />} />
         </Route>
 
       </Routes>
